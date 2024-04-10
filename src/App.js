4cbbf6fc5d62f590e76553fc9ec8719ae6d143bc
RoterHund <foxton.alan@gmail.com>
2024-04-10 09:42:18 -0400

    added react components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
 import Landing_Page from './Components/Landing_Page/Landing_Page';
+import Login from './Components/Login/Login';
+import Sign_Up from './Components/Sign_Up/Sign_Up';
 
 function App() {
 
@@ -11,6 +13,8 @@ function App() {
           <Navbar/>
               <Routes>
                 <Route path="/" element={<Landing_Page />} />
+                <Route path="/" element={<Login />} />
+                <Route path="/" element={<Sign_Up />} />
               </Routes>          
         </BrowserRouter>      
     </div>
