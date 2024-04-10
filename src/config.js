b7eba16c7052e031686681e6e9c568b24610c45c
RoterHund <foxton.alan@gmail.com>
2024-04-10 08:27:20 -0400

    add links to index.html

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const API_URL = window.location.hostname === "localhost" ? "https://labs.cognitiveclass.ai/v2/tools/cloud-ide-kubernetes?ulid=ulid-ac0352ec8e3f81bdc76912f829ae5ece15b9332f" : "add your theia server side url";
+console.log(
+    "API_URL :",
+    API_URL
+);
