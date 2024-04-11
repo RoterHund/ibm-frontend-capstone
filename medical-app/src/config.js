6111def4cac81666ca5d39a218d830e703d40b6b
RoterHund <foxton.alan@gmail.com>
2024-04-11 06:43:45 -0400

    update config files

--- medical-app/src/config.js
+++ medical-app/src/config.js
@@ -1,4 +1,4 @@
-export const API_URL = window.location.hostname === "localhost" ? "https://labs.cognitiveclass.ai/v2/tools/cloud-ide-kubernetes?ulid=ulid-ac0352ec8e3f81bdc76912f829ae5ece15b9332f" : "add your theia server side url";
+export const API_URL = window.location.hostname === "localhost" ? "https://foxtonalan-8181.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai" : "add your theia server side url";
 console.log(
     "API_URL :",
     API_URL
