f23e6472e42ea38053ffb313860e3742401c36d1
RoterHund <foxton.alan@gmail.com>
2024-04-11 10:16:40 -0400

    added instant consultation

--- medical-app/src/App.js
+++ medical-app/src/App.js
@@ -5,6 +5,7 @@ import Navbar from './Components/Navbar/Navbar'; // Import Navbar component
 import Landing_Page from './Components/Landing_Page/Landing_Page'; // Import Landing Page component
 import Login from './Components/Login/Login'; // Import Login component
 import Sign_Up from './Components/Sign_Up/Sign_Up'; // Import Sign Up component
+import InstantConsultation from './Components/InstantConsultation/InstantConsultation'; // Import Sign Up component
 
 // Define the App component as the main application entry point
 function App() {
@@ -26,6 +27,7 @@ function App() {
           <Route path="/" element={<Landing_Page />} /> {/* Route for home page */}
           <Route path="/login" element={<Login />} /> {/* Route for login page */}
           <Route path="/signup" element={<Sign_Up />} /> {/* Route for sign up page */}
+          <Route path="/instant-consultation" element={<InstantConsultation />} /> {/* Route for instant consultation page */}
         </Routes>
 
       </BrowserRouter>
