91bbaefafb56d11ae00035125f998076d510e44f
RoterHund <foxton.alan@gmail.com>
2024-05-01 18:59:21 -0400

    added seo keywords

--- medical-app/server/db.js
+++ medical-app/server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:MzgwNS1mb3h0b25h@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:MTM0MzktZm94dG9u@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
